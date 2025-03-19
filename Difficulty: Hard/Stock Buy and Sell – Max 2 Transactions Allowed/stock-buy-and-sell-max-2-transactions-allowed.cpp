30ab28eec2edd683e2710150f36109efa8aa7d0d
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-03-20 04:05:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.cpp
+++ Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.cpp
@@ -7,30 +7,55 @@ using namespace std;
 
 class Solution {
   public:
-    int maxProfit(vector<int> &prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2+1, vector<int>(2, 0)));
+    // int dp[10001][2][3];
+
+    // int solve(vector<int>& arr, int idx, bool buy, int trans) {
+    //     if (trans == 2 || idx == arr.size()) return 0;
+
+    //     if (dp[idx][buy][trans] != -1)
+    //         return dp[idx][buy][trans];
+
+    //     int skip = solve(arr, idx + 1, buy, trans);
+    //     int profit = 0;
+
+    //     if (buy) {  // Buy stock
+    //         profit = solve(arr, idx + 1, false, trans) - arr[idx];
+    //     } else {  // Sell stock
+    //         profit = solve(arr, idx + 1, true, trans + 1) + arr[idx];
+    //     }
+
+    //     return dp[idx][buy][trans] = max(skip, profit);
+    // }
+
+    int maxProfit(vector<int> &arr) {
+        // if (arr.empty()) return 0;
+        // memset(dp, -1, sizeof(dp));
+        // return solve(arr, 0, true, 0);
+        
+        int n=arr.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         
         for(int idx = n-1; idx >= 0; idx--) {
-            for(int t = 1; t <= 2; t++) {
-                for(int buy = 0; buy <= 1; buy++) {
-                    int skip = dp[idx+1][t][buy];
-        
-                    int take = 0;
-                    if(buy) {
-                        take = dp[idx+1][t][0] - prices[idx];
-                    } else {
-                        take = dp[idx+1][t-1][1] + prices[idx];
+            for(int buy = 1; buy >= 0; buy--) {
+                for(int trans = 1; trans >= 0; trans--) {
+                    int skip = dp[idx+1][buy][trans];
+                    int profit = 0;
+            
+                    if (buy) {  // Buy stock
+                        profit = dp[idx+1][0][trans] - arr[idx];
+                    } else {  // Sell stock
+                        profit = dp[idx+1][1][trans+1] + arr[idx];
                     }
-                    
-                    dp[idx][t][buy] = max(skip, take);
+            
+                    dp[idx][buy][trans] = max(skip, profit);
                 }
             }
         }
         
-        return max(dp[0][1][1], dp[0][2][1]);
+        return dp[0][1][0];
     }
 };
+
 
 
 //{ Driver Code Starts.
