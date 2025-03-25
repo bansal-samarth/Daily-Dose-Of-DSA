068768b143734df6474a5fa35f59b2f2ee111076
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-03-25 16:13:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     int maximumProfit(vector<int> &prices) {
-        int n = prices.size();
         
         int mini = prices[0];
-        int maxi = 0;
-        
-        for(int i = 1; i < n; i++) {
-            maxi = max(maxi, prices[i] - mini);
+        int profit = 0;
+        for(int i = 1 ; i < prices.size(); i++) {
+            profit = max(profit, prices[i] - mini);
             mini = min(mini, prices[i]);
         }
         
-        return maxi;
+        return profit;
     }
 };
 
