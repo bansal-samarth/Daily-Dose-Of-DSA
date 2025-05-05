20016073f9b8f659881b996d54e65f18a336a426
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-05-05 07:20:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -7,27 +7,39 @@ using namespace std;
 
 class Solution {
   public:
-    int dp[1001][1001];
-    int solve(vector<int>& coins, int sum, int idx) {
-        if(sum == 0)
-            return 1;
-        if(idx == coins.size())
-            return 0;
+    // int solve(vector<int>& coins, int sum, int idx) {
+    //     if(sum == 0)
+    //         return 1;
+    //     if(idx == coins.size())
+    //         return 0;
         
-        if(dp[sum][idx] != -1)
-            return dp[sum][idx];
+    //     if(dp[sum][idx] != -1)
+    //         return dp[sum][idx];
         
-        int skip = solve(coins, sum, idx+1);
+    //     int skip = solve(coins, sum, idx+1);
         
-        int take = 0;
-        if(sum >= coins[idx])
-            take = solve(coins, sum - coins[idx], idx);
+    //     int take = 0;
+    //     if(sum >= coins[idx])
+    //         take = solve(coins, sum - coins[idx], idx);
         
-        return dp[sum][idx] = skip + take;
-    }
+    //     return dp[sum][idx] = skip + take;
+    // }
+    
     int count(vector<int>& coins, int sum) {
-        memset(dp, -1, sizeof(dp));
-        return solve(coins, sum, 0);
+        // memset(dp, -1, sizeof(dp));
+        // return solve(coins, sum, 0);
+        
+        vector<int> dp(sum+1, 0);
+        dp[0] = 1;
+        
+        for(int coin : coins) {
+            for(int amount = 1; amount <= sum; amount++) {
+                if(coin <= amount)
+                    dp[amount] += dp[amount - coin];
+            }
+        }
+        
+        return dp[sum];
     }
 };
 
