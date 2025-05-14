39f07c4481fc68fb785b0d6f8cfcbe4ca38e3656
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-05-14 21:57:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.cpp
+++ Difficulty: Medium/Look and Say Pattern/look-and-say-pattern.cpp
@@ -1,53 +1,48 @@
 //{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h> 
+// Initial template for C++
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
- public:
-    string rec(string& str) {
-        int n = str.size();
-        string res = "";
-        
-        int count = 1;
-        for(int i = 0; i < n; i++) {
-            if(str[i] != str[i+1] || n == i+1) {
-                res += to_string(count) + string(1, str[i]);
-                count = 1;
-            } else
-                count++;
+  public:
+    string countAndSay(int n) {
+        // code here
+        // code here
+        string res="1", temp="";
+        for(int i=1; i<n; i++){
+            int count=1;
+            for(int j=0; j<res.size(); j++){
+                while(res[j]==res[j+1]){
+                    count++;
+                    j++;
+                }
+                temp = temp+to_string(count)+res[j];
+                count=1;
+            }
+            res=temp;
+            temp="";
         }
-        
         return res;
     }
-    string lookandsay(int n) {
-        
-        string ans = "1";
-        
-        for(int i = 1; i < n; i++) {
-            ans = rec(ans);
-        }
-        
-        return ans;
-    }   
 };
 
 
 //{ Driver Code Starts.
 int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
+        cin >> n;
         Solution obj;
-        cout<<obj.lookandsay(n)<<endl;
-    
-cout << "~" << "\n";
-}
+        cout << obj.countAndSay(n) << endl;
+
+        cout << "~"
+             << "\n";
+    }
     return 0;
 }
 // } Driver Code Ends
