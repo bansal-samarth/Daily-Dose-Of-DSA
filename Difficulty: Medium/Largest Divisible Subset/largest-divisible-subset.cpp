b308c8d972e161115d94f6f5ce737c93804e6bfe
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-06-22 23:45:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest Divisible Subset/largest-divisible-subset.cpp
@@ -0,0 +1,68 @@
+class Solution {
+  public:
+        vector<int> largestSubset(vector<int>& arr) {
+        int n = arr.size(), mx = 0;
+        vector<vector<int>> dp(n);
+        vector<int> ans;
+        sort(arr.begin(), arr.end());
+        
+        for(int i = 0; i < n; i++){
+            dp[i] = {arr[i]};
+            for(int j = 0; j < i; j++) {
+                if (1 + dp[j].size() >= dp[i].size() and arr[i] % arr[j] == 0) {
+                    vector<int> temp = dp[i];
+                    temp.pop_back();
+                    if (1 + dp[j].size() ==  dp[i].size()) {
+                        temp = dp[j];
+                        temp.push_back(arr[i]);
+                        dp[i] = max(dp[i], temp);
+                    } 
+                    else {
+                        dp[i] = dp[j];
+                        dp[i].push_back(arr[i]);
+                    }
+                }
+            }
+            mx = max(mx, (int) dp[i].size());
+        }
+        
+        for (int i = 0; i < n; i++){
+            if(dp[i].size() == mx) {
+                if(ans.empty())
+                    ans = dp[i];
+                ans = max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+  
+    // void solve(vector<int>& arr, vector<int>& output, vector<int>& ans, int prev, int idx) {
+    //     if(idx == arr.size()) {
+    //         if(output.size() > ans.size()) {
+    //             ans = output;
+    //         }
+    //         return;
+    //     }
+        
+    //     // SKIP
+    //     solve(arr, output, ans, prev, idx + 1);
+        
+    //     // TAKE
+    //     if(prev == -1 || (arr[idx] % arr[prev] == 0)) {
+    //         output.push_back(arr[idx]);
+    //         solve(arr, output, ans, idx, idx + 1);
+    //         output.pop_back();
+    //     }
+    // }
+    
+    // vector<int> largestSubset(vector<int>& arr) {
+    //     sort(arr.begin(), arr.end());
+        
+    //     vector<int> output;
+    //     vector<int> ans;
+        
+    //     solve(arr, output, ans, -1, 0);
+        
+    //     return ans;
+    // }
+};
