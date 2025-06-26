3bfa482b13dc62b4f7ca72550c05839b69f7e668
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-06-27 00:49:12 +0530

    Time: 3 ms (83.99%), Space: 11.4 MB (86.63%)06-27-2025

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> map;
-
+        
+        vector<int> freq(216, 0);
         int maxi = 0;
 
         int left = 0;
         for(int right = 0; right < s.size(); right++) {
-            map[s[right]]++;
+            freq[s[right]]++;
 
-            while(left < right && map[s[right]] > 1) {
-                map[s[left]]--;
-                left++;
+            while(freq[s[right]] > 1) {
+                --freq[s[left++]];
             }
 
             maxi = max(maxi, right - left + 1);
