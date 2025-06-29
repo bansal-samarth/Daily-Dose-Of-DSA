7e0aabf37e2dce706dfb7b75a8c0ce4d05722c07
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-06-30 01:14:26 +0530

    Time: 4 ms (68.45%), Space: 20.2 MB (31.8%)06-30-2025

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,79 +1,39 @@
-// class Solution {
-// public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         vector<vector<int>> adj(numCourses); 
-//         vector<int> indegree(numCourses, 0); 
-        
-//         for(auto& prerequisite : prerequisites) {
-//             int course = prerequisite[0];
-//             int prereq = prerequisite[1];
-//             adj[prereq].push_back(course);
-//             indegree[course]++;
-//         }
-
-//         queue<int> q;
-
-//         for(int i = 0; i < numCourses; i++) {
-//             if(indegree[i] == 0)
-//                 q.push(i);
-//         }
-
-//         vector<int> order;
-//         while(!q.empty()) {
-//             int val = q.front();
-//             q.pop();
-//             order.push_back(val);
-
-//             for(int course : adj[val]) {
-//                 if(--indegree[course] == 0) {
-//                     q.push(course);
-//                 }
-//             }
-//         }
-
-//         if(order.size() == numCourses)
-//             return true;
-//         return false;
-//     }
-// };
-
-
 class Solution {
 public:
-    bool isCycle(int node,vector<int> &vis,vector<int> adj[],vector<int> &rec) {  
-        vis[node]=1;
-        rec[node]=1;
-
-        for(int child : adj[node]) {
-            if(vis[child] == 0) {
-                if(isCycle(child,vis,adj,rec))
-                    return true;
-            }
-            else {
-                if(rec[child]) return true;
-            }
-        }
-
-        rec[node] = 0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        vector<int> rec(numCourses,0);
-        vector<int> vis(numCourses, 0);
-       
-        for(int i=0;i<prerequisites.size();i++) {
-            int a=prerequisites[i][0];
-            int b=prerequisites[i][1];
-            adj[b].push_back(a); 
-        }
         
-        for(int i=0;i<numCourses;i++) {
-            if(!vis[i]) {
-                if(isCycle(i,vis,adj,rec))
-                    return false;
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        
+        for(auto pre : prerequisites) {
+            int u = pre[0];
+            int v = pre[1];
+
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++)
+            if(indegree[i] == 0)
+                q.push(i);
+        
+        vector<int> ans;
+
+        while(!q.empty()) {
+            int course = q.front();
+            q.pop();
+
+            ans.push_back(course);
+
+            for(int learn : adj[course]) {
+                indegree[learn]--;
+
+                if(indegree[learn] == 0)
+                    q.push(learn);
             }
-        } 
-        return true;
+        }
+
+        return ans.size() == numCourses;
     }
 };
