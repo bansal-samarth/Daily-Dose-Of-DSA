bf819373b88a1e7dde6b036bf7a8ae8ee40bb175
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-07-02 06:59:58 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.88%)07-02-2025

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        
-        int j = -1;
-        string ans = "";
+    string reverseWords(string str) {
+        reverse(str.begin(), str.end());
 
-        bool start = false;
+        int n = str.size();
+        int left = 0;
+        int right = 0;
 
-        int x;
-        for (x = s.size() - 1; x >= 0 && s[x] == ' '; x--) {
-            // Empty body, the loop will terminate when a non-space character is found.
+        int i = 0;
+        while(i < n) {
+            
+            while(i < str.size() && str[i] == ' ')
+                i++;
+
+            if (i == n)
+                break;
+
+            while(i < str.size() && str[i] != ' ')
+                str[right++] = str[i++];
+            
+            reverse(str.begin() + left, str.begin() + right);
+
+            str[right++] = ' ';
+            left = right;
         }
-
-        int y;
-        for (y = 0; y < s.size() && s[y] == ' '; y++) {
-            // Empty body, the loop will terminate when a non-space character is found.
-        }
-
-        s = s.substr(y, x + 1);
-
-        cout<<"."<<s<<"."<<endl;
-
-        for(int i = 0 ; i < s.size(); i++)
-        {
-            if(s[i] == ' ' && start == true)
-            {
-                ans = s.substr(j,i-j) + ans;
-                j = -1;
-                while(i+1 < s.size() && s[i]==' ' && s[i+1]==' ')
-                    i++;
-                
-                if(i != s.size()-1)
-                    ans = " "+ ans;
-            }
-            else
-            {
-                start = true;
-                if(j == -1)
-                    j = i;
-            }
-        }
-        if(j != -1)
-            ans = s.substr(j,s.size()-j) + ans;
         
-        return ans;
-        
+        str.resize(right - 1);
+        return str;
     }
 };
