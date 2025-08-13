9b5073edf266f67f70c69e3cbf27689dd90360ac
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-08-13 09:02:43 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.32%)08-13-2025

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-
         int left = 0;
-        int right = n - 1;
+        int right = height.size() - 1;
 
-        int maxL = height[0];
-        int maxR = height[n - 1];
+        int maxL = height[left];
+        int maxR = height[right];
 
         int water = 0;
 
@@ -15,8 +13,7 @@ public:
             if(maxL < maxR) {
                 water += maxL - height[left++];
                 maxL = max(maxL, height[left]);
-            }
-            else {
+            } else {
                 water += maxR - height[right--];
                 maxR = max(maxR, height[right]);
             }
