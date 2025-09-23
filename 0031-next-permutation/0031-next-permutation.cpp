57f5ceadb85deac1081488d2e5a819c42657bf8b
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-09-24 02:00:27 +0530

    Time: N/A (0%), Space: N/A (0%)09-24-2025

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int stop = -1;
-
+        int change = -1;
+        
         int n = nums.size();
-        for(int i = n - 1; i > 0; i--) {
-            if(nums[i - 1] < nums[i]) {
-                stop = i - 1;
+        for(int i = n - 1 ; i > 0; i--) {
+            if(nums[i] > nums[i - 1]) {
+                change = i - 1;
                 break;
             }
         }
 
-        if(stop != -1) {
-            int val = nums[stop];
-            
-            int i = n - 1;
-            for(; i > stop; i--) {
-                if(val < nums[i])
-                    break;
+        if(change != -1) {
+            for(int i = n - 1; i >= 0; i--) {
+                if(nums[change] < nums[i]) {
+                    swap(nums[change], nums[i]);
+                }
             }
-
-            swap(nums[stop], nums[i]);
         }
 
-        sort(nums.begin() + stop + 1, nums.end());
-
-        return;
+        sort(nums.begin() + change + 1, nums.end());
     }
 };
