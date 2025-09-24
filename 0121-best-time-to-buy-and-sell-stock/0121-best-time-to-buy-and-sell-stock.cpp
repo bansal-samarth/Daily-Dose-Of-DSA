c5dda7f152a4ea12659b65266d0be576afec2463
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-09-25 00:35:13 +0530

    Time: 0 ms (100%), Space: 97.1 MB (98.99%)09-25-2025

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int prof = 0;
+
+        int mini = INT_MAX;
+
+        for(int i = 0; i < prices.size(); i++) {
+            mini = min(mini, prices[i]);
+            prof = max(prof, prices[i] - mini);
+        }
+
+        return prof;
+    }
+};
