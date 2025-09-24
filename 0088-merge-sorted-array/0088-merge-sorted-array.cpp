3def293bb3af906a2f0893a8375ee7519bc29ca4
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-09-25 00:54:52 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.64%)09-25-2025

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,14 +1,18 @@
 class Solution {
- public:
+public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      // nums1's index (the actual nums)
-        int j = n - 1;      // nums2's index
-        int k = m + n - 1;  // nums1's index (the next filled position)
+        int size = m + n - 1;
+        m--;
+        n--;
 
-        while (j >= 0)
-        if (i >= 0 && nums1[i] > nums2[j])
-            nums1[k--] = nums1[i--];
-        else
-            nums1[k--] = nums2[j--];
+        while(size >= 0) {
+            if(n == -1 || (m != -1 && (nums1[m] > nums2[n]))) {
+                nums1[size--] = nums1[m--];
+            } else {
+                nums1[size--] = nums2[n--];
+            }
+        }
+
+        return;
     }
 };
