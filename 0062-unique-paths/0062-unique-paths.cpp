5979cce379882ff444bddd3c7adac274d700c95b
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2025-09-27 12:21:34 +0530

    Time: 1 ms (24.77%), Space: 9.4 MB (8.95%)09-27-2025

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int solve(int i, int j, int m, int n) {
+        if(i == m-1 && j == n-1)
+            return 1;
+        
+        if(i >= m || j >= n)
+            return 0;
+        
+        int right = solve(i, j + 1, m, n);
+        int down = solve(i + 1, j, m, n);
+
+        return right + down;
+    }
     int uniquePaths(int m, int n) {
+
+        // return solve(0, 0, m, n);
 
         if(m == 1 || n == 1) return 1;
         
