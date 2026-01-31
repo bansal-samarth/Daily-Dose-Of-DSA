422e7f925732b4453cfb2a8a14d9fc4f3a7a2887
Samarth Bansal <115192873+bansal-samarth@users.noreply.github.com>
2026-01-31 15:22:12 +0530

    Time: 5 ms (80.49%), Space: 65.5 MB (46.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -6,11 +6,9 @@ function isPalindrome(x: number): boolean {
     let rev = 0;
 
     while(num != 0) {
-        let rem = num % 10; // 1   -> 2
-        rev = rev*10 + rem; // 1   -> 1*10 + 2 = 12
-        num /= 10;          // 12 -> 1 
-        num = Math.floor(num);
-        console.log(rem, rev, num);
+        let rem = num % 10;
+        rev = rev*10 + rem;
+        num = Math.floor(num/10);
     }
 
     return rev === x;
